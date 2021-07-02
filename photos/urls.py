f75a2e826d3f3c1b1b274fb73fbc722155d09a37
ndanu-josy <josphinendanu3@gmail.com>
2021-07-02 21:31:35 +0300

    Install project requirements,  update settings and urls

--- photos/urls.py
+++ photos/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
   
     url(r'^$',views.home,name='home'),
-    url(r'^galllery$',views.my_projects,name='myGallery'),
+    url(r'^galllery$',views.my_gallery,name='myGallery'),
     
     
 
