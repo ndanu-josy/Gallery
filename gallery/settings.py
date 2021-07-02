778a81dda58560feecb2fdddc771e61ea0ee77c2
ndanu-josy <josphinendanu3@gmail.com>
2021-07-02 21:51:00 +0300

    Category and location models then perform migration

--- gallery/settings.py
+++ gallery/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',   
+        'USER': 'moringa',      
+        'PASSWORD':'josie',
     }
 }
 
@@ -120,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
--- photos/models.py
+++ photos/models.py
@@ -1,6 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)        
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def get_image_locations(cls):
+        locations = Location.objects.all()
+        return locations      
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()  
+
 class Image(models.Model):
     image = models.ImageField(upload_to='images/')
     name = models.CharField(max_length=100)
@@ -16,3 +40,7 @@ class Image(models.Model):
 
     def delete_image(self):
         self.delete()
+
+
+        
+                  
